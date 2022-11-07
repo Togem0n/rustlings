aa57ec9e217bb570c57bae5a3ff3c5511ca2a66c
Togem0n <kai.wang@futuregames.nu>
2022-11-07 13:47:23 +0100

    rustlings

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: usize) -> usize{
+    let total: usize;
+    if num <= 40 {
+        total = num * 2; 
+    } else {
+        total = num;
+    } 
+    total
+}
 
 // Don't modify this function!
 #[test]
